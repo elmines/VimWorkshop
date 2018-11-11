05c2f9e44a6519c94c3ae7482117ed20d594a90a
Ethan Mines <elmines325@gmail.com>
2018-11-10 21:36:01 -0600

    Write intro file

---
+++ intro.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(void){
+
+printf("Press h to go left, j to go down, k to go up, l to go right\n");
+printf("Press i to go into INSERT mode and actually type stuff\n");
+printf("Press ESC when in INSERT mode to leave it\n");
+
+printf("                  kkk                 \n");
+printf("                 kkkkk                \n");
+printf("                kkkkkkk               \n");
+printf("               kkkkkkkkk              \n");
+printf("              kkkkkkkkkkk             \n");
+printf("                  kkk                 \n");
+printf("                  kkk                 \n");
+printf("                  kkk                 \n");
+printf("                  kkk                 \n");
+
+printf("   h                              l   \n");
+printf("  hh                              ll  \n");
+printf(" hhh                              lll \n");
+printf("hhhhhhhhh                    lllllllll\n");
+printf("hhhhhhhhh                    lllllllll\n");
+printf("hhhhhhhhh                    lllllllll\n");
+printf(" hhh                              lll \n");
+printf("  hh                              ll  \n");
+printf("   h                              l   \n");
+
+printf("                  jjj                 \n");
+printf("                  jjj                 \n");
+printf("                  jjj                 \n");
+printf("                  jjj                 \n");
+printf("              jjjjjjjjjjj             \n");
+printf("               jjjjjjjjj              \n");
+printf("                jjjjjjj               \n");
+printf("                 jjjjj                \n");
+printf("                  jjj                 \n");
+
+
+return 0;
+
+}
