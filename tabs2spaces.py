c4b792a17049470813e14c3a3edc6485accedae6
Ethan Mines <elmines325@gmail.com>
2018-11-12 22:03:26 -0600

    Fix tabs2spaces.py

--- tabs2spaces.py
+++ tabs2spaces.py
@@ -1,15 +1,15 @@
 
 for i in range(10):
-    if i % 2 == 0:
-		print("Aren't people who use tabs instead of spaces utterly loathsome.?")
+	if i % 2 == 0:
+                print("Aren't people who use tabs instead of spaces utterly loathsome.?")
 
-	if i % 2 == 1:
-                print("I'd hate to be the sucker who has to fix this file's whitespace.")	
+        if i % 2 == 1:
+		print("I'd hate to be the sucker who has to fix this file's whitespace.")
 
 #In some spots I've used 4 spaces, and in other spots I've used a tab
 
 #If we want to change from tabs to spaces. . .
-#Type :%s/\t/ \    /gc
+#Type :%s/\t/        /gc
 
 #If we want to change from spaces to tabs. . .
-#Type :%s/ \{4}/\t/gc
+#Type :%s/ \{8}/\t/gc
